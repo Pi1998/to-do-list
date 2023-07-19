b8a0f213e45dafe17f3d0308337063ff9c1e09a2
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-07-20 02:29:45 +0630

    Add functions(add, remove, save, update index) update renderTasks function, empty array

--- src/index.js
+++ src/index.js
@@ -1,43 +1,100 @@
 // import _ from 'lodash';
 import './style.css';
 
-const tasks = [
-  {
-    description: 'Task 1',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Task 2',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Task 3',
-    completed: false,
-    index: 3,
-  },
-];
+// Get references to the HTML elements
+const toDoList = document.getElementById('tdl-lists');
+const addNewListForm = document.getElementById('tdl-add');
+const addNewListInput = document.getElementById('tdl-add-input');
+const clearTaskBtn = document.getElementById('clear-btn');
 
+// Define the key for local storage
+const Local_Storage_Task_Key = 'task.tasks';
+
+// Retrieve tasks from local storage or create an empty array if no tasks are stored
+let tasks = JSON.parse(localStorage.getItem(Local_Storage_Task_Key)) || [];
+
+// Add event listener to the form for adding a new task
+addNewListForm.addEventListener('submit', e => {
+  e.preventDefault();
+  const description = addNewListInput.value;
+  if (description == null || description === '') return;
+
+  // Create a new task object and add it to the tasks array
+  const task = addTask(description);
+  addNewListInput.value = null;
+  tasks.push(task);
+
+  // Save and render the updated tasks list
+  saveAndRender();
+});
+
+// Add event listener to the "Clear all completed" button
+clearTaskBtn.addEventListener('click', e => {
+  tasks = tasks.filter(task => !task.completed); // Remove completed tasks from the tasks array
+  updateIndexes(); // Update the indexes of remaining tasks
+  saveAndRender();
+});
+
+
+// Function to create a new task object
+function addTask(description) {
+  return {
+    description: description,
+    completed: false,
+
+    // Set the index based on the current length of the tasks array
+    index: tasks.length + 1,
+  };
+}
+
+// Update the indexes of remaining tasks after a task is deleted
+function updateIndexes() {
+  tasks.forEach((task, index) => {
+    task.index = index + 1;
+  });
+}
+
+function saveAndRender() {
+  save();
+  renderTasks();
+}
+
+// Function to save the tasks to local storage
+function save() {
+  localStorage.setItem(Local_Storage_Task_Key, JSON.stringify(tasks));
+}
+
+
+// Function to render the tasks list
 function renderTasks() {
-  const todoList = document.getElementById('tdl-lists');
-  todoList.innerHTML = '';
+  toDoList.innerHTML = '';
 
-  tasks.forEach((task) => {
+  tasks.forEach(task => {
     const listItem = document.createElement('div');
     listItem.classList.add('tdl-item');
 
     const checkbox = document.createElement('input');
     checkbox.classList.add('check-box');
     checkbox.type = 'checkbox';
+    checkbox.index = task.index;
     checkbox.checked = task.completed;
+
     checkbox.addEventListener('change', () => {
       task.completed = checkbox.checked;
+
+      // Re-render the tasks to apply the strike-through style for completed tasks
       renderTasks();
     });
 
     const description = document.createElement('span');
     description.textContent = task.description;
+
+    // Check if the task is completed and apply the style with strike-through if true
+    if (task.completed) {
+      description.style.textDecoration = 'line-through';
+    } else {
+      description.style.textDecoration = 'none';
+    }
 
     const optionIcon = document.createElement('span');
     optionIcon.innerHTML = '<button><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>';
@@ -46,8 +103,10 @@ function renderTasks() {
     listItem.appendChild(checkbox);
     listItem.appendChild(description);
     listItem.appendChild(optionIcon);
-    todoList.appendChild(listItem);
+    toDoList.appendChild(listItem);
   });
 }
 
-document.addEventListener('DOMContentLoaded', renderTasks);
+
+// Event listener to render the tasks list when the page is loaded
+document.addEventListener('DOMContentLoaded', saveAndRender);
