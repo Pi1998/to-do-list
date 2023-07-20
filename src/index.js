48cf0c1c5b9f8499e47cf2a20621f9acd3952bda
Shinn Thant Swam Ye <1shinnthantswamye@gmail.com>
2023-07-20 19:56:31 +0630

    Add editTask function, editTaskDescription function, update renderTask, add removeTask function

--- src/index.js
+++ src/index.js
@@ -14,6 +14,64 @@ const LOCAL_STORAGE_TASK_KEY = 'task.tasks';
 // Retrieve tasks from local storage or create an empty array if no tasks are stored
 let tasks = JSON.parse(localStorage.getItem(LOCAL_STORAGE_TASK_KEY)) || [];
 
+function editTask(index) {
+  const listItem = toDoList.children[index - 1];
+  const description = listItem.querySelector('span');
+  const optionIcon = listItem.querySelector('.option-icon');
+
+  // Make the task description editable
+  const textBox = document.createElement('input');
+  textBox.classList.add('txt-box');
+  textBox.type = 'text';
+  textBox.value = description.textContent;
+
+  // Style the input box to remove the outline (border) on focus
+  textBox.style.outline = 'none';
+
+  // Replace the description with the editable text box
+  listItem.replaceChild(textBox, description);
+
+  // Show the trash can icon
+  optionIcon.innerHTML = '<button><i class="fa fa-trash-can" aria-hidden="true"></i></button>';
+
+  // Add event listener to save the edited task on pressing Enter
+  textBox.addEventListener('keyup', (e) => {
+    if (e.key === 'Enter') {
+      const newDescription = textBox.value;
+      editTaskDescription(index, newDescription);
+    }
+  });
+
+  // Add event listener to save the edited task when focus is lost from the text box
+  textBox.addEventListener('blur', () => {
+    const newDescription = textBox.value;
+    editTaskDescription(index, newDescription);
+  });
+
+  // Show the trash can icon and add event listener to it for removing the task
+  const trashIcon = document.createElement('i');
+  trashIcon.classList.add('fa', 'fa-trash-can');
+  optionIcon.innerHTML = '';
+  optionIcon.appendChild(trashIcon);
+  trashIcon.addEventListener('click', () => {
+    removeTask(index);
+  });
+}
+
+function editTaskDescription(index, newDescription) {
+  const task = tasks.find((task) => task.index === index);
+  if (task) {
+    task.description = newDescription;
+    saveAndRender();
+    // Get the listItem and optionIcon elements
+    const listItem = toDoList.children[index - 1];
+    const optionIcon = listItem.querySelector('.option-icon');
+
+    // Set the optionIcon HTML back to the option icon
+    optionIcon.innerHTML = '<button><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>';
+  }
+}
+
 // Function to render the tasks list
 function renderTasks() {
   toDoList.innerHTML = '';
@@ -48,6 +106,11 @@ function renderTasks() {
     const optionIcon = document.createElement('span');
     optionIcon.innerHTML = '<button><i class="fa fa-ellipsis-v" aria-hidden="true"></i></button>';
     optionIcon.classList.add('option-icon');
+
+    // Add event listener to the task description for editing
+    listItem.addEventListener('dblclick', () => {
+      editTask(task.index); // Call editTask function when the listItem is clicked
+    });
 
     listItem.appendChild(checkbox);
     listItem.appendChild(description);
@@ -91,6 +154,12 @@ function addTask(description) {
   };
 }
 
+// function to remove individual task
+function removeTask(index) {
+  tasks = tasks.filter((task) => task.index !== index); // Remove the task with the specified index
+  updateIndexes(); // Update the indexes of remaining tasks
+  saveAndRender();
+}
 // Add event listener to the form for adding a new task
 addNewListForm.addEventListener('submit', (e) => {
   e.preventDefault();
